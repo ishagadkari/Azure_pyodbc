4c07b5d0f9a8139b489c67c1df0efd10f5285f4a
ishagadkari <isha.gadkari@gmail.com>
2023-09-10 13:06:21 -0500

    updates

--- Azure_testing.py
+++ Azure_testing.py
@@ -31,7 +31,33 @@ def adding_table(query_file_path, cursor, connection):
     except pyodbc.Error as e:
         print(f"Error executing query: {e}")
 
-#closing the cursor and the connection. 
+'''
+def table(table_name, columns, cursor, connection):
+    try:
+        create_table_query = f"CREATE TABLE {table_name} ("
+        for column_name, data_type in columns:
+            create_table_query += f"[{column_name}] {data_type}, "
+        create_table_query = create_table_query.rstrip(', ')
+        create_table_query += ")"
+        cursor.execute(create_table_query)
+        connection.commit()
+    except pyodbc.Error as e:
+        print(f"Error executing query: {e}")
+'''
+
+
+def adding_rows(table_name, rows, cursor, connection):
+    try:
+        insert_query = f"INSERT INTO {table_name} VALUES ({', '.join(['?'] * len(rows_to_insert[0]))})"
+        for row in rows:
+            cursor.execute(insert_query, row)
+
+        # Commit the changes
+        connection.commit()
+    except pyodbc.Error as e:
+        print(f"Error adding rows to the table: {e}")
+
+#closing the cursor and the connection.
 def close_connection(cursor, connection):
     # Close the cursor
      cursor.close()
@@ -44,5 +70,9 @@ if __name__ == "__main__":
     es = establish_string("ishasandbox1.database.windows.net", "ishaSandbox", "isha.gadkari", "Bhopal123456")
     connection = establish_connection(es)
     cursor = create_cursor(connection)
-    adding_table("C:\\Users\\ishag\\Documents\\Azure_testing\\Query2.sql", cursor, connection)
+    #adding_table("C:\\Users\\ishag\\Documents\\Azure_testing\\Query2.sql", cursor, connection)
+    #columns = [('ID', 'INT'), ('Name', 'VARCHAR(255)'), ('Age', 'INT')]
+    #table("new_table", columns, cursor, connection)
+    rows_to_insert = [('J', 30, 1), ('A', 25, 1)]
+    adding_rows("Table2", rows_to_insert, cursor, connection)
     close_connection(cursor, connection)
